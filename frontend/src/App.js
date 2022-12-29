b53e027d4a9d86acdd5077271759fdb05868d58c
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2022-12-29 21:47:48 +0200

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import {useEffect, useState} from "react";
+
+const App = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('/api/users').then(value => value.json()).then(value => setUsers(value))
+    }, [])
+
+    return (
+        <div>
+            <h1>Users:</h1>
+            {users.map(user=><div key={user.id}>{JSON.stringify(user)}</div>)}
+        </div>
+    );
+};
+
+export {App};
